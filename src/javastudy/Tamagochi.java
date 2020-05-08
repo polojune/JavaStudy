0c122dbe1e1d0ce2cc5f17974221c1845a6c7aa4
jjk <crossfit@a.naver.com>
2020-05-08 17:42:45 +0900

    다마고치1

--- src/javastudy/Tamagochi.java
+++ src/javastudy/Tamagochi.java
@@ -1,6 +1,7 @@
 package javastudy;
 
 import java.awt.Graphics;
+import java.util.Calendar;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -8,19 +9,18 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
-
-
-class loginView extends JFrame {
+class loginView extends JFrame implements Runnable {
 	private JButton b1, b2, b3, b4;
 	JScrollPane scrollPane;
 	ImageIcon b = null;
 	ImageIcon c = null;
- 
-	
+	Thread th;
+
 	public loginView() {
+		th = new Thread(this);
 		b = new ImageIcon("img/a.jpg");
 		// c = new ImageIcon("img/c.gif");
-
+		
 		JPanel panel = new JPanel() {
 			public void paintComponent(Graphics g) {
 				g.drawImage(b.getImage(), 0, 0, null);
@@ -46,12 +46,11 @@ class loginView extends JFrame {
 		setTitle("다마고치 만들기");
 		// Container c = getContentPane();
 		panel.setLayout(null);
-		JButton b1 = new JButton("밥먹기");
-		JButton b2 = new JButton("운동하기");
-		JButton b3 = new JButton("잠자기");
-		JButton b4 = new JButton("14:04:05");
-        
-		
+		b1 = new JButton("밥먹기");
+		b2 = new JButton("운동하기");
+		b3 = new JButton("잠자기");
+		b4 = new JButton("14:04:05");
+
 		b1.setBounds(0, 302, 168, 35);
 		b2.setBounds(170, 302, 160, 35);
 		b3.setBounds(331, 302, 145, 35);
@@ -61,23 +60,38 @@ class loginView extends JFrame {
 		panel.add(b2);
 		panel.add(b3);
 		panel.add(b4);
-      
-      
+
 		setSize(492, 376);
 		// setLocationRelativeTo(null);
+		th.start();
 		setVisible(true);
 	}
 
+	@Override
+	public void run() {
+		while (true) {
+
+			Calendar c = Calendar.getInstance();
+			b4.setText(c.get(Calendar.HOUR) + ":"+c.get(Calendar.MINUTE)+":"+c.get(Calendar.SECOND));
+			repaint();
+
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+
+			}
+
+		}
+
+	}
+
 }
-
-
-
 
 public class Tamagochi {
 
 	public static void main(String[] args) {
 		new loginView();
-		
+
 	}
 
 }
