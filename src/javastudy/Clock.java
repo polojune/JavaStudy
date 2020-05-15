5591ef710f6946172a5d0f4408ce7e0bedf00552
jjk <crossfit@a.naver.com>
2020-05-15 17:00:52 +0900

    다마고치V2.0

--- src/javastudy/Clock.java
+++ src/javastudy/Clock.java
@@ -1,59 +1,62 @@
 package javastudy;
 
+import java.awt.Color;
+import java.awt.Container;
 import java.awt.Font;
-import java.util.Calendar;
+import java.time.LocalTime;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Clock extends JFrame {
+
+	private JLabel laTime;
+	private JPanel paTime;
+
 	public Clock() {
-		setTitle("시계보기");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		getContentPane().add(new Lbi());
-		setSize(500, 500);
+		getContentPane().setBackground(Color.LIGHT_GRAY);
+		Container c = getContentPane();
+
+		laTime = new JLabel("14:04:05");
+		laTime.setBounds(50, 10, 170, 54);
+		laTime.setFont(new Font("맑은 고딕", Font.PLAIN, 40));
+		paTime = new JPanel();
+		paTime.setBounds(12, 79, 260, 76);
+		paTime.setBackground(Color.WHITE);
+		paTime.setLayout(null);
+		paTime.add(laTime);
+		setSize(300, 300);
+		setLocationRelativeTo(null);
+		getContentPane().setLayout(null);
+		c.add(paTime);
+
+		Thread t1 = new Thread(new TimerThread());
+		t1.start();
 		setVisible(true);
 	}
 
-	class Lbi extends JLabel implements Runnable {
-		Thread timeThread = null;
-
-		public Lbi() {
-			setText(setClockText());
-			setFont(new Font("fname", Font.ITALIC, 70));
-			setHorizontalAlignment(JLabel.CENTER);
-			timeThread = new Thread(Lbi.this);
-			timeThread.start();
-		}
-
-		public String setClockText() {
-			Calendar c = Calendar.getInstance();
-			int hour = c.get(Calendar.HOUR);
-			int min = c.get(Calendar.MINUTE);
-			int second = c.get(Calendar.SECOND);
-
-			String clockTxt = Integer.toString(hour);
-			clockTxt = clockTxt.concat(":");
-			clockTxt = clockTxt.concat(Integer.toString(min));
-			clockTxt = clockTxt.concat(":");
-			clockTxt = clockTxt.concat(Integer.toString(second));
-			return clockTxt;
-		}
-
+	class TimerThread implements Runnable {
 		@Override
 		public void run() {
+			// 시간이 변하게 하기
 			while (true) {
+				String currentTime = LocalTime.now().toString();
+				currentTime = currentTime.split("[.]")[0];
 				try {
 					Thread.sleep(1000);
+					laTime.setText(currentTime);
 				} catch (Exception e) {
-					return;
+					e.printStackTrace();
 				}
-				setText(setClockText());
-			}
 
+			}
 		}
 	}
+
 	public static void main(String[] args) {
-		 new Clock();
+		new Clock();
 	}
+
 }
--- src/javastudy/Tamagochi.java
+++ src/javastudy/Tamagochi.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.time.LocalTime;
 import java.util.Calendar;
 
 import javax.swing.ImageIcon;
@@ -21,27 +22,40 @@ class LoginView extends JFrame /* implements Runnable */ {
 	private JButton b1, b2, b3, b4;
 	JScrollPane scrollPane;
 	ImageIcon backIcon = null;
+	ImageIcon backIcon1 =null;
 	ImageIcon characterIcon = null;
 	ImageIcon characterIcon1 = null;
-	JLabel lbCharacter;
+	ImageIcon characterIcon2 = null;
+	ImageIcon food = null;
+	ImageIcon zzz[];
+	// JLabel lbCharacter;
 	private CharacterPanel panel;
 	Thread th;
 	Calendar c;
 	int x, y;
-	boolean a = true;
+	int foodX, foodY;
+	Timer timer;
+	Timer zzzTimer;
+	// 기본상태 0, 밥먹기 1, 운동하기 2, 잠자기 3
 
 	public LoginView() {
+		foodX = 0;
+		foodY = 0;
 		x = 150;
 		y = 150;
 		// th = new Thread(this);
 		backIcon = new ImageIcon("img/a.jpg");
+		backIcon1 = new ImageIcon("img/night.jpg");
 		characterIcon = new ImageIcon("img/k1.png");
-		characterIcon1 = new ImageIcon("img/k2.png");
+		characterIcon1 = new ImageIcon("img/k3.png");
+		characterIcon2 = new ImageIcon("img/sleep.png");
+		food = new ImageIcon("img/rice1.png");
 
-		lbCharacter = new JLabel(characterIcon);
-		lbCharacter.setSize(200, 200);
-		lbCharacter.setLocation(x, y);
-		panel = new CharacterPanel(characterIcon, backIcon, x, y);
+//		lbCharacter = new JLabel(characterIcon);
+//		lbCharacter.setSize(200, 200);
+//		lbCharacter.setLocation(x, y);
+		// panel = new CharacterPanel(characterIcon, backIcon, x, y);
+		panel = new CharacterPanel(characterIcon, backIcon, food);
 
 		/*
 		 * panel = new JPanel() { public void paintComponent(Graphics g) {
@@ -54,16 +68,44 @@ class LoginView extends JFrame /* implements Runnable */ {
 		 * } };
 		 */
 		// panel.add(lbCharacter);
-		scrollPane = new JScrollPane(panel);
-		setContentPane(scrollPane);
+		// scrollPane = new JScrollPane(panel);
+		// setContentPane(scrollPane);
 
 		setTitle("다마고치 만들기");
 		// Container c = getContentPane();
 		panel.setLayout(null);
 		b1 = new JButton("밥먹기");
+		b1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				timer.stop();
+				panel.setStatus(1);
+				panel.setImage(characterIcon);
+				panel.setBackImage(backIcon);
+			}
+		});
+
 		b2 = new JButton("운동하기");
+		b2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				timer.start();
+				panel.setStatus(2);
+				panel.setImage(characterIcon1);
+				panel.setBackImage(backIcon);
+			}
+		});
 		// b2.addActionListener(new MyActionListener());
 		b3 = new JButton("잠자기");
+		b3.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				timer.stop();
+				panel.setStatus(3);
+				panel.setImage(characterIcon2);
+				panel.setBackImage(backIcon1);
+			}
+		});
 		b4 = new JButton("14:04:05");
 
 		b1.setBounds(0, 302, 168, 35);
@@ -76,7 +118,7 @@ class LoginView extends JFrame /* implements Runnable */ {
 		panel.add(b3);
 		panel.add(b4);
 
-		// panel.add(panel);
+		setContentPane(panel);
 
 		setSize(500, 376);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -86,37 +128,52 @@ class LoginView extends JFrame /* implements Runnable */ {
 
 		new CurrentTime().runTime(b4);
 
-		b2.addMouseListener(new MouseAdapter() {
+//		b2.addMouseListener(new MouseAdapter() {
+//
+//			@Override
+//			public void mouseClicked(MouseEvent e) {
+//				if (a) {
+//					a = false;
+//				} else {
+//					a = true;
+//				}
+//				repaint();
+//			}
+//		});
 
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				if (a) {
-					a = false;
-				} else {
-					a = true;
-				}
-				repaint();
-			}
-		});
-
-		Timer timer = new Timer(20, new ActionListener() {
+		timer = new Timer(20, new ActionListener() {
 			int delta = 3;
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (x > 300) {
-					delta = (-1 * delta);
-				} else if (x < 0) {
-					delta = (-1 * delta);
-				}
-
-				x = x + delta;
+				// if (chrStatus == 2) {
+					if (x > 300) {
+						delta = (-1 * delta);
+					} else if (x < 0) {
+						delta = (-1 * delta);
+					}
+					x = x + delta;
+				//  }
 
 				panel.setPos(x, y);
+				//panel.foodPos(x, y);
 
 			}
 		});
-		timer.start();
+		
+		zzzTimer = new Timer(600, new ActionListener() {
+					int index = 0;
+			@Override
+			public void actionPerformed(ActionEvent e) {
+					
+					if (index >2 ) index = 0;
+				    panel.setZzzIndex(index);
+				    index++;
+				    repaint();
+			}
+		});
+		
+		zzzTimer.start();
 	}
 
 //	class MyActionListener implements ActionListener {
@@ -131,47 +188,6 @@ class LoginView extends JFrame /* implements Runnable */ {
 //		}
 //	}
 
-	class CharacterPanel extends JPanel {
-		private ImageIcon characterImage;
-		private ImageIcon backImage;
-		private int x;
-		private int y;
-
-		public CharacterPanel() {
-
-		}
-
-		public CharacterPanel(ImageIcon icon) {
-			this.characterImage = icon;
-		}
-
-		public CharacterPanel(ImageIcon icon, ImageIcon backImage, int x, int y) {
-			//super();
-			this.characterImage = icon;
-			this.backImage = backImage;
-			this.x = x;
-			this.y = y;
-		}
-
-		public void setPos(int x, int y) {
-			this.x = x;
-			this.y = y;
-			repaint();
-		}
-
-		public void setImage(ImageIcon icon) {
-			this.characterImage = icon;
-		}
-
-		public void paintComponent(Graphics g) {
-			// System.out.println("paintComponent 실행");
-			g.drawImage(backImage.getImage(), 0, 0, null);
-			g.drawImage(characterImage.getImage(), x, y, 150, 150, null);
-			if(a) {
-				 g.drawImage(characterIcon.getImage(), 150, 150, 150, 150, null); }else {
-				 g.drawImage(characterIcon1.getImage(), 150, 150, 150, 150, null); }
-		}
-	}
 //	@Override
 //	public void run() {
 //		while (true) {
@@ -200,14 +216,115 @@ class CurrentTime {
 			@Override
 			public void run() {
 				while (true) {
-					Calendar c = Calendar.getInstance();
-					button.setText(c.get(Calendar.HOUR) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND));
+					String currentTime = LocalTime.now().toString();
+					currentTime = currentTime.split("[.]")[0];
+					button.setText(currentTime);
+					
+				//					Calendar c = Calendar.getInstance();
+//					button.setText(c.get(Calendar.HOUR) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND));
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
 
 			}
 		}).start();
 	}
 
+}
+
+class CharacterPanel extends JPanel {
+	private ImageIcon characterImage;
+	private ImageIcon backImage;
+	private ImageIcon foodImage;
+	private ImageIcon zzz[];
+	private int x = 150;
+	private int y = 150;
+	private int foodX;
+	private int foodY;
+	private int zzzX;
+	private int zzzY;
+
+	private int status;
+	private int zzzIndex;
+	
+//	public CharacterPanel() {
+//
+//	}
+
+//	public CharacterPanel(ImageIcon icon) {
+//		this.characterImage = icon;
+//	}
+
+//	public CharacterPanel(ImageIcon icon, ImageIcon backImage, int x, int y) {
+//		//super();
+//		this.characterImage = icon;
+//		this.backImage = backImage;
+//		this.x = x;
+//		this.y = y;
+//	}
+	
+	public CharacterPanel(ImageIcon icon, ImageIcon backImage, ImageIcon foodImage) {
+		// super();
+		int status = 1;
+		this.characterImage = icon;
+		this.backImage = backImage;
+		this.foodImage = foodImage;
+		
+		zzz = new ImageIcon[3];
+		for (int i = 0; i < zzz.length; i++) {
+			zzz[i] = new ImageIcon("img/z"+i+".png");
+		}
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public void setPos(int x, int y) {
+		this.x = x;
+		this.y = y;
+		repaint();
+	}
+
+	public void foodPos(int x, int y) {
+		this.foodX = x;
+		this.foodY = y;
+		repaint();
+	}
+
+	public void setImage(ImageIcon icon) {
+		this.characterImage = icon;
+		repaint();
+	}
+	public void setBackImage(ImageIcon icon) {
+		this.backImage = icon;
+		repaint();
+	}
+	
+	public void setZzzIndex(int zzzIndex) {
+		this.zzzIndex = zzzIndex;
+	}
+
+	public void paintComponent(Graphics g) {
+		// super.paintComponent(g);
+		//System.out.println("x, y :" + this.x + " " + this.y);
+		g.drawImage(backImage.getImage(), 0, 0, null);
+		g.drawImage(characterImage.getImage(), x, y, 150, 150, null);
+		if(status==1) {
+			g.drawImage(foodImage.getImage(), 20, 60, 100, 100, null);
+		}
+		if(status==3) {
+				g.drawImage(zzz[zzzIndex].getImage(), x+100, y-100, 150, 150, null);
+		}
+		
+//		if(a) {
+//			 g.drawImage(characterIcon.getImage(), 150, 150, 150, 150, null); }else {
+//			 g.drawImage(characterIcon1.getImage(), 150, 150, 150, 150, null); }
+	}
 }
 
 //class MovingThread {
